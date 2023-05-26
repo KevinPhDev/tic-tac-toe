96a522613b05661552ccc048759dad57326f2c66
KevinPhDev <kevinphdev@outlook.com>
2023-05-26 16:05:16 +1000

    Add a click event listener to display the 'X' marker on each square clicked

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-const gameBoard = {
-    board: ['', '', '', '', '', '', '', '', '']
+const gameBoard = (() => {
+    const board = ['', '', '', '', '', '', '', '', '']
+    return {
+        board
+    }
+})();
+
+const createPlayer = function (name, marker) {
+    return {
+        name: name,
+        marker: marker
+    }
 }
 
-const createPlayer = function (name) {
-    return {
-        name: name
-    }
+const player1 = createPlayer('playerOne', 'X');
+const player2 = createPlayer('playerTwo', 'O');
+
+const squares = document.querySelectorAll('.square');
+
+squares.forEach(square => {
+    square.addEventListener('click', () => setMarker(square))
+})
+
+function setMarker(square) {
+    square.innerHTML = 'X';
 }
