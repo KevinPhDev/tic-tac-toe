964c7e36a924e122294b63bd8136df13cd88a07c
KevinPhDev <kevinphdev@outlook.com>
2023-05-31 20:35:29 +1000

    Check winning marker to increase a winning player's points by 1

--- script.js
+++ script.js
@@ -9,25 +9,30 @@ const gameBoard = (() => {
         if (board[4] !== '' && board[4] === board[0] && board[0] === board[8]) {
             console.log('Diagonal Win 1');
             console.log(`${board[4]} Wins!`);
+            return board[4];
         }
         if (board[4] !== '' && board[4] === board[2] && board[2] === board[6]) {
             console.log('Diagonal Win 2');
             console.log(`${board[4]} Wins!`);
+            return board[4];
         }
     }
 
     const horizontalWin = () => {
         if (board[0] !== '' && board[0] == board[1] && board[1] == board[2]) {
             console.log('Horizontal Win 1');
-            console.log(`${board[0]} Wins!`)
+            console.log(`${board[0]} Wins!`);
+            return board[0];
         }
         if (board[3] !== '' && board[3] == board[4] && board[4] == board[5]) {
             console.log('Horizontal Win 2');
             console.log(`${board[3]} Wins!`);
+            return board[3];
         }
         if (board[6] !== '' && board[6] == board[7] && board[7] == board[8]) {
             console.log('Horizontal Win 3');
             console.log(`${board[6]} Wins!`);
+            return board[6];
         }
     }
 
@@ -35,14 +40,17 @@ const gameBoard = (() => {
         if (board[0] !== '' && board[0] == board[3] && board[3] == board[6]) {
             console.log('Vertical Win 1');
             console.log(`${board[0]} Wins!`);
+            return board[0];
         }
         if (board[1] !== '' && board[1] == board[4] && board[4] == board[7]) {
             console.log('Vertical Win 2');
             console.log(`${board[1]} Wins!`);
+            return board[1];
         }
         if (board[2] !== '' && board[2] == board[5] && board[5] == board[8]) {
             console.log('Vertical Win 3');
             console.log(`${board[2]} Wins!`);
+            return board[2];
         }
     }
 
@@ -55,15 +63,16 @@ const gameBoard = (() => {
     }
 })();
 
-const createPlayer = function (name, marker) {
+const createPlayer = function (name, marker, points) {
     return {
         name: name,
-        marker: marker
+        marker: marker,
+        points: points
     }
 }
 
-const playerOne = createPlayer('playerOne', 'X');
-const playerTwo = createPlayer('playerTwo', 'O');
+const playerX = createPlayer('playerX', 'X', 0);
+const playerO = createPlayer('playerO', 'O', 0);
 
 const squares = document.querySelectorAll('.square');
 
@@ -78,9 +87,15 @@ squares.forEach(square => {
         if (gameBoard.isBoardFull()) {
             console.log('Board is full');
         }
-        gameBoard.diagonalWin();
-        gameBoard.horizontalWin();
-        gameBoard.verticalWin();
+        if (gameBoard.diagonalWin() || gameBoard.horizontalWin() || gameBoard.verticalWin() === 'X') {
+            playerX.points++;
+            console.log(`X: ${playerX.points}`);
+            console.log(`O: ${playerO.points}`);
+        } else if (gameBoard.diagonalWin() || gameBoard.horizontalWin() || gameBoard.verticalWin() === 'O') {
+            playerO.points++;
+            console.log(`X: ${playerX.points}`);
+            console.log(`O: ${playerO.points}`);
+        }
     });
 });
 
